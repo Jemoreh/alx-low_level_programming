57028e0f04e7f6562f6ccff7a81ae6d30fd72b73
Jemoreh <jaymutisya102@gmail.com>
2022-03-10 07:52:15 -0800

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 
-/**
 *main - prints the phrase "Programming is the building a multilingual puzzle"
 *Return: 0 if exited properly, non-zero otherwise
 */
 int main(void)
-{
-.   puts("\"Programming is the building a multilingual puzzle");
+{puts("\"Programming is the building a multilingual puzzle");
 return(0);
 }
