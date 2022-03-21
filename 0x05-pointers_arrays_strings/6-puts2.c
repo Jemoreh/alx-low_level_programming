29d6729d86a94ca7e5135d1808cd5685113e4066
root <jaymutisya102@gmail.com>
2022-03-21 15:22:45 -0700

    puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,7 +3,7 @@
 /**
 *puts2 - prints one char out of2 of a string
 *followed by a new line
-*@str: string to print the chars from 
+*@str:string to print the charsfrom 
 */
 void puts2(char *str)
 {
@@ -13,8 +13,7 @@ while (str[len] != '\0')
 {
 len++;
 }
-for
-(i = 0; i < len; i += 2)
+for (i = 0; i < len; i += 2)
 {
 _putchar(str[i]);
 }
